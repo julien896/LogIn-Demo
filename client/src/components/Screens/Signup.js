462446b3205c7cdca4c7504e2dbee1619472e50e
Julien <julicogui@gmail.com>
2020-05-24 02:22:53 -0300

    login

--- client/src/components/Screens/Signup.js
+++ client/src/components/Screens/Signup.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Signup = () => {
     return ( 
-        <h1>Signup</h1>
+        <div className="mycard">
+            <div className="card auth-card">
+            <h2>Sign Up</h2>
+            <input
+            type="text"
+            placeholder="Name"
+            />
+            <input
+            type="text"
+            placeholder="Email"
+            />
+            <input
+            type="text"
+            placeholder="Password"
+            />
+            <button className="btn waves-effect waves-light #0d47a1 blue darken-4" type="submit">
+                Sign Up
+            </button>
+            <h5>
+            <Link to='./signin'> Already have an account? </Link>
+            </h5>
+            </div>
+        </div>
+        
      );
 }
  
 export default Signup;
+
+
+//
